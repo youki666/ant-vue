4e34b6b086b179ad50c93db340cc220d38c774bd
youki <xiaohuihui1335@163.com>
2020-01-14 17:28:15 +0800

    递归文件取不到title

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="width: 256px">
+    <a-button type="primary" @click="toggleCollapsed" style="margin-bottom: 16px">
+      <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <a-menu
+            :defaultSelectedKeys="['1']"
+            :defaultOpenKeys="['2']"
+            mode="inline"
+            theme="dark"
+            :inlineCollapsed="collapsed"
+            @click="clicked"
+            @select="selected"
+    >
+      <template v-for="item in list">
+        <a-menu-item v-if="!item.children" :key="item.key">
+          <a-icon type="pie-chart" />
+          <span>{{item.title}}</span>
+        </a-menu-item>
+        <sub-menu v-else :menu-info="item" :key="item.key" />
+      </template>
+    </a-menu>
+    <div style="margin-left: 200px;">
+      <img src="./assets/menu@2x.png" alt="" style="width: 200px;height: 200px;">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  /*
+   * recommend SubMenu.vue https://github.com/vueComponent/ant-design-vue/blob/master/components/menu/demo/SubMenu.vue
+   * SubMenu1.vue https://github.com/vueComponent/ant-design-vue/blob/master/components/menu/demo/SubMenu1.vue
+   * */
+  import SubMenu from './components/SubMenu';
+  export default {
+    components: {
+      'sub-menu': SubMenu,
+    },
+    data() {
+      return {
+        collapsed: false,
+        list: [
+          {
+            key: '1',
+            title: 'Option 1',
+          },
+          {
+            key: '2',
+            title: 'Navigation 2',
+            children: [
+              {
+                key: '2.1',
+                title: 'Navigation 3',
+                children: [{ key: '2.1.1', title: 'Option 2.1.1' }],
+              },
+            ],
+          },
+        ],
+      };
+    },
+    methods: {
+      toggleCollapsed() {
+        this.collapsed = !this.collapsed;
+      },
+      selected({item}){
+        console.log(item)
+      },
+      clicked({item}){
+        console.log(item)
+      }
+    },
+  };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+import App from './App';
+import './plugins/ant'
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
